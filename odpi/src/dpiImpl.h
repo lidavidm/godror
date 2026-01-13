3ca713aa19b8f2ad14d2216c88066e695a50913a
David Li <li.davidm96@gmail.com>
2026-01-13 13:57:13 +0900

    [EXP] Allow direct access to data buffers

--- odpi/src/dpiImpl.h
+++ odpi/src/dpiImpl.h
@@ -2358,4 +2358,7 @@ int dpiUtils__setAccessTokenAttributes(void *handle,
 void dpiDebug__initialize(void);
 void dpiDebug__print(const char *format, ...);
 
+typedef dpiDataBuffer DpiDataBuffer;
+typedef struct { int IsNull; DpiDataBuffer Value; } DpiDataDefinition;
+
 #endif
